7aa5231f751de1daa01f29a8c569725003380be1
Patrick Longo <ck.longo@bol.com.br>
2019-10-30 13:12:18 -0300

    Done

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -16,7 +16,7 @@ class PostList extends Component {
           avatar: imageA,
         },
         date: '04 Jun 2019',
-        content: 'Pessoal, alguém sabe se a Rocketseat está contratando?',
+        content: 'Pessoal, o que vocês estão achando do conteudo do bootcamp da RocketSeat?',
         comments: [
           {
             id: 1,
@@ -25,8 +25,7 @@ class PostList extends Component {
               avatar: imageB,
             },
             date: '04 Jun 2019',
-            content:
-              'A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)'
+            content: 'Simplesmente #Delicinha',
           }
         ]
       },
@@ -37,8 +36,7 @@ class PostList extends Component {
           avatar: imageD,
         },
         date: '04 Jun 2019',
-        content:
-          'Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia',
+        content: 'Fala galera, beleza?\ncomo vocês estão psicológicamente, enquanto estão fazendo o bootcamp da RocketSeat?',
         comments: [
           {
             id: 1,
@@ -47,8 +45,7 @@ class PostList extends Component {
               avatar: imageF,
             },
             date: '04 Jun 2019',
-            content:
-              'Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!'
+            content:'Simplesmente #FullDopamina',
           },
           {
             id: 2,
@@ -57,8 +54,7 @@ class PostList extends Component {
               avatar: imageC,
             },
             date: '04 Jun 2019',
-            content:
-              'Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes'
+            content:'#Delicinha',
           }
         ]
       },
